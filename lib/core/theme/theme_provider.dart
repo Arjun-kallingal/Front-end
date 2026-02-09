9c73fdd66082f08d2e4dffa510e42d47048aa195
Arjun Kallingal <arjunkallingal96@gmail.com>
2026-02-09 11:26:12 +0530

    Update theme_provider.dart

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
-import 'dark_theme.dart';
-import 'light_theme.dart';
 
-class ThemeProvider extends ChangeNotifier {
-  bool _isDark = true;
-
-  ThemeData get theme =>
-      _isDark ? DarkTheme.theme : LightTheme.theme;
-
-  void toggleTheme() {
-    _isDark = !_isDark;
-    notifyListeners();
-  }
-}
