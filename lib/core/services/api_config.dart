e2199e92dec32b2c28590ead5c92974cef1f105b
Syamjith-ms <syamjith7077@gmail.com>
2026-03-27 15:21:54 +0530

    .

--- lib/core/services/api_config.dart
+++ lib/core/services/api_config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 
 class ApiConfig {
 
-  static const String _localIp = "192.168.0.182";
+  static const String _localIp = "192.168.29.128";
   static const String _port = "5000";
 
   static String get baseUrl {
